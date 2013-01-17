bac7bf8aeffed4c7423a4f78c0f1a7fe42dfddbc
FTP User <199447-63-webdav@m12s2-1-22db.ispgateway.de>
2013-01-17 21:59:33 +0100

    [INIT] initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "vantomas".
+ *
+ * Auto generated 17-01-2013 21:01
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'van-tomas.de Websitepackage',
+	'description' => '',
+	'category' => 'fe',
+	'author' => 'Thomas Juhnke',
+	'author_email' => 'tommy@van-tomas.de',
+	'shy' => '',
+	'dependencies' => 'cms,extbase,fluid',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"768a";s:12:"ext_icon.gif";s:4:"1bdc";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"0ce4";s:20:"doc/wizard_form.html";s:4:"9a12";}',
+);
+
+?>
