7fe2b2396ddbd1f9393bb9e9d27bbed1b2b53ec9
Thomas Juhnke <tommy@van-tomas.de>
2013-02-05 16:27:53 +0100

    [TASK] adding mobile unit for last updated pages list

--- Configuration/TypoScript/Setup/Mobile/Extensions/css_styled_content.ts
+++ Configuration/TypoScript/Setup/Mobile/Extensions/css_styled_content.ts
@@ -8,30 +8,3 @@ tt_content.stdWrap.innerWrap.cObject.default >
 
 // ...
 tt_content.menu.20.1.wrap = <ul data-role="listview" data-inset="true">|</ul>
-
-// -- jQuery mobile compatible setup for "subpages with abstract"
-tt_content.menu.20.4.wrap = <div data-role="collapsible-set">|</div>
-
-tt_content.menu.20.4.1.alternativeSortingField = lastUpdated desc
-tt_content.menu.20.4.1.maxItems = 5
-
-tt_content.menu.20.4.1.NO.after >
-tt_content.menu.20.4.1.NO.before.cObject = COA
-tt_content.menu.20.4.1.NO.before.cObject {
-	10 = TEXT
-	10 {
-		field = nav_title // title
-		wrap = <h3>|</h3>
-	}
-	20 = TEXT
-	20 {
-		data = field:abstract // field:description // field:subtitle
-		required = 1
-		htmlSpecialChars = 0
-		parseFunc =< lib.parseFunc_RTE
-	}
-	wrap = <div data-role="collapsible" data-collapsed="true">|
-}
-tt_content.menu.20.4.1.NO.linkWrap = |</div>
-tt_content.menu.20.4.1.NO.stdWrap.override = read more
-tt_content.menu.20.4.1.NO.ATagParams = data-role="button" data-icon="arrow-r" data-iconpos="right" data-theme="b"
