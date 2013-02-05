359a375c3dbc70c52a2cb06932cb8572d640507e
Thomas Juhnke <tommy@van-tomas.de>
2013-02-05 16:58:24 +0100

    [TASK] removing wt_twitter static file inclusion

--- Configuration/TypoScript/Constants/Desktop/index.ts
+++ Configuration/TypoScript/Constants/Desktop/index.ts
@@ -3,7 +3,6 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:comments/static/constants.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:comments_gravatar/static/comments__gravatar/constants.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vge_tagcloud/static/constants.txt">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:wt_twitter/Configuration/TypoScript/Main/constants.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:beautyofcode/pi1/static/constants.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:sk_fancybox/static/constants.txt">
 
@@ -16,7 +15,6 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/comments.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/comments_gravatar.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/sp_socialbookmarks.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/wt_twitter.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/form4_pages_rss.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/beautyofcode.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/vantomas.ts">
--- Configuration/TypoScript/Setup/Desktop/index.ts
+++ Configuration/TypoScript/Setup/Desktop/index.ts
@@ -6,7 +6,6 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:comments/static/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:comments_gravatar/static/comments__gravatar/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vge_tagcloud/static/setup.txt">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:wt_twitter/Configuration/TypoScript/Main/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:beautyofcode/pi1/static/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:sk_fancybox/static/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:sp_socialbookmarks/static/setup.txt">
@@ -23,7 +22,6 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Setup/Desktop/Extensions/comments.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Setup/Desktop/Extensions/comments_gravatar.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Setup/Desktop/Extensions/sp_socialbookmarks.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Setup/Desktop/Extensions/wt_twitter.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Setup/Desktop/Extensions/form4_pages_rss.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Setup/Desktop/Extensions/beautyofcode.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Setup/Desktop/Extensions/vantomas.ts">
