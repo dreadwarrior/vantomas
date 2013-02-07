3bbd873ef689abb6b3b3c5ddecd2f0d12e54959a
Thomas Juhnke <tommy@van-tomas.de>
2013-02-07 20:08:24 +0100

    [TASK] removing contact form TypoScript in favor of include static in BE

--- Configuration/TypoScript/Constants/Mobile/contact.ts
+++
@@ -1,8 +0,0 @@
-# cat=form/request; type=string; label=Anfrageformular: E-Mailsender, kann eine E-Mailadresse oder "fromField" sein
-form.request.emailSender = fromField
-# cat=form/request; type=string; label=Anfrageformular: E-Mailempfänger, muss eine gültige E-Mailadresse sein
-form.request.emailReceiver = tommy@van-tomas.de
-# cat=form/request; type=string; label=Anfrageformular: Antwort an, kann eine E-Mailadresse oder "fromField" sein
-form.request.emailReplyTo = fromField
-# cat=form/request; type=options[Normal=normal]; label=Anfrageformular: Typ der Betreffzeile
-form.request.subjectType = normal
--- Configuration/TypoScript/Setup/Desktop/contact.ts
+++
@@ -1,1 +0,0 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:form/Configuration/TypoScript/setup.txt">
