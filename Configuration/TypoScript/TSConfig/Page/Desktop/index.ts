44eb8ce8473a2fd784af6ee9322fd5d3f6e55d31
Thomas Juhnke <tommy@van-tomas.de>
2013-02-07 19:59:47 +0100

    [TASK] unifying RTE TSConfig based on duplicate detection

--- Configuration/TypoScript/TSConfig/Page/Desktop/index.ts
+++ Configuration/TypoScript/TSConfig/Page/Desktop/index.ts
@@ -1,3 +1,4 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Desktop/tcemain.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Desktop/mod.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Desktop/tceform.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/rte.ts">
