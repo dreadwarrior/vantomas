b823a30396e0e5398234f93bbc47d7d9f24a6f83
Thomas Juhnke <tommy@van-tomas.de>
2013-02-08 16:36:53 +0100

    [TASK] removing empty TypoScript constant files

--- Configuration/TypoScript/Constants/Desktop/Extensions/sk_fancybox.ts
+++
@@ -1,4 +0,0 @@
-plugin.sk_fancybox.padding = 1
-plugin.sk_fancybox.hideOnContentClick = 0
-plugin.sk_fancybox.overlayColor = #272727
-plugin.sk_fancybox.overlayOpacity = 0.3
--- Configuration/TypoScript/Constants/Desktop/index.ts
+++ Configuration/TypoScript/Constants/Desktop/index.ts
@@ -7,19 +7,10 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:sk_fancybox/static/constants.txt">
 
 // -- extension override
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/cwmobileredirect.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/ja_replacer.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/css_styled_content.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/sk_fancybox.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/vge_tagcloud.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/comments.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/comments_gravatar.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/sp_socialbookmarks.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/form4_pages_rss.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/beautyofcode.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/Extensions/vantomas.ts">
 
 // -- custom setup, libraries etc.
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/sitemap_xml.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/statistics.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Desktop/root.ts">
