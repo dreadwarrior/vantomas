8354d28ae55023b01624679330dd0cec662e5a01
Thomas Juhnke <tommy@van-tomas.de>
2013-02-08 16:30:07 +0100

    [TASK] removing duplicate Page TSConfig mod.* setting

--- Configuration/TypoScript/TSConfig/Page/Mobile/index.ts
+++ Configuration/TypoScript/TSConfig/Page/Mobile/index.ts
@@ -1,4 +1,4 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Mobile/tcemain.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Mobile/mod.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/mod.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Mobile/tceform.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/rte.ts">
