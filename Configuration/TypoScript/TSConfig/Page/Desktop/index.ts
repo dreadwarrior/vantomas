4aca514e3016a7616f345244ba12e750b13123bb
Thomas Juhnke <tommy@van-tomas.de>
2013-02-08 17:04:15 +0100

    [TASK] introducing Default namespace to page TSConfig

--- Configuration/TypoScript/TSConfig/Page/Desktop/index.ts
+++ Configuration/TypoScript/TSConfig/Page/Desktop/index.ts
@@ -1,4 +1,4 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Desktop/tcemain.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/mod.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Default/mod.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Desktop/tceform.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/rte.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/Default/rte.ts">
