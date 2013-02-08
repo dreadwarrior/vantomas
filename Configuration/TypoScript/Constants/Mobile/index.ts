b5dae7fcd6a7aa70bd386c23b26b68830cff5c52
Thomas Juhnke <tommy@van-tomas.de>
2013-02-08 16:38:23 +0100

    [TASK] removing empty TypoScript constant files for mobile

--- Configuration/TypoScript/Constants/Mobile/index.ts
+++ Configuration/TypoScript/Constants/Mobile/index.ts
@@ -6,11 +6,8 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:beautyofcode/pi1/static/constants.txt">
 
 // -- extension override
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Mobile/Extensions/css_styled_content.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Mobile/Extensions/comments.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Mobile/Extensions/beautyofcode.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Mobile/Extensions/vantomas.ts">
 
 // -- custom setup, libraries etc.
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Mobile/sitemap_xml.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/Constants/Mobile/root.ts">
