79cee67e9907b3cfba8a6d6d7ad990bec8dc3164
Thomas Juhnke <tommy@van-tomas.de>
2013-02-10 19:25:19 +0100

    [TASK] ext:dialog template + TS configuration adjustments

--- Configuration/TypoScript/Setup/Default/Extensions/dialog.ts
+++ Configuration/TypoScript/Setup/Default/Extensions/dialog.ts
@@ -1,8 +1,15 @@
 plugin.tx_dialog {
 	settings {
+		enableExpandingTextareas = 0
+
 		attachments {
 			files.enable = 0
 			images.enable = 0
+		}
+
+		email {
+			fromEmail = comments@van-tomas.de
+			fromName = van-tomas.de Comment form
 		}
 	}
 }
