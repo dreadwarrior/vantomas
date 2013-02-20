ee9ddb652fe59c5eede8eecf3ba7d33d33a73eb2
Thomas Juhnke <tommy@van-tomas.de>
2013-02-20 14:51:23 +0100

    [TASK] moving ext:beautyofcode CSS into external files

--- Configuration/TypoScript/Setup/Mobile/Extensions/beautyofcode.ts
+++ Configuration/TypoScript/Setup/Mobile/Extensions/beautyofcode.ts
@@ -1,15 +1,4 @@
 // "Consolas", "Bitstream Vera Sans Mono", "Courier New", Courier, monospace
 plugin.tx_beautyofcode_pi1.jQueryOnReadyCallback = (document).on('pageinit', 'div:jqmData(role="page")', function (event) {
 
-plugin.tx_beautyofcode_pi1._CSS_DEFAULT_STYLE (
-.ui-page .tx_beautyofcode_pi1 pre { overflow: auto; }
-.ui-page .syntaxhighlighter .lines { padding: 8px !important; }
-.ui-page .syntaxhighlighter, .ui-page .syntaxhighlighter div, .ui-page .syntaxhighlighter code, 
-.ui-page .syntaxhighlighter table, .ui-page .syntaxhighlighter table td, .ui-page .syntaxhighlighter table tr, 
-.ui-page .syntaxhighlighter table tbody {
-	font-family: "Droid Sans Mono", monospace !important;
-	line-height: 1.4em !important;
-	font-size: 1em !important;
-}
-.ui-page .syntaxhighlighter .line .content { text-shadow: none; }
-)
+plugin.tx_beautyofcode_pi1._CSS_DEFAULT_STYLE >
