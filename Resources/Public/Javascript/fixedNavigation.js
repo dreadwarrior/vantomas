81f8d41ba2df2faf959f862e2e575cca0dfb56c4
Thomas Juhnke <tommy@van-tomas.de>
2013-03-04 22:53:57 +0100

    [TASK] adding more mobile first to the navigation + logo

--- Resources/Public/Javascript/fixedNavigation.js
+++ Resources/Public/Javascript/fixedNavigation.js
@@ -2,19 +2,12 @@
 	var
 		headerHeight = $('.w-header').height(),
 		navigationHeight = $('.w-header nav').height(),
-		headerOffset = headerHeight - navigationHeight,
-		cssHash = {};
+		headerOffset = headerHeight - navigationHeight;
 
 	$(window).scroll(function () {
 		if ($(window).scrollTop() > headerOffset) {
-			cssHash = {
-				'top': '-' + headerOffset + 'px'
-			};
-
-			$('.w-header').css(cssHash);
 			$('body').addClass('fixed-header');
 		} else {
-			$('.w-header').css(cssHash);
 			$('body').removeClass('fixed-header');
 		}
 	});
