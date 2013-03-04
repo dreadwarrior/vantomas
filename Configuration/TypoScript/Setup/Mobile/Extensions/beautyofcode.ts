c6d90a87ea231f629aeb6b70bcf233ea08f8bc65
Thomas Juhnke <tommy@van-tomas.de>
2013-03-04 20:41:36 +0100

    [CLEANUP] mobile: removing TypoScript

--- Configuration/TypoScript/Setup/Mobile/Extensions/beautyofcode.ts
+++
@@ -1,4 +0,0 @@
-// "Consolas", "Bitstream Vera Sans Mono", "Courier New", Courier, monospace
-plugin.tx_beautyofcode_pi1.jQueryOnReadyCallback = (document).on('pageinit', 'div:jqmData(role="page")', function (event) {
-
-plugin.tx_beautyofcode_pi1._CSS_DEFAULT_STYLE >
