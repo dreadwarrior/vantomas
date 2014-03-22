141a275e0ece7781846d88b4d56bf8fd5777a0cb
Tommy Juhnke <tommy@van-tomas.de>
2014-03-22 16:55:55 +0100

    [TASK] restructuring TSConfig

--- Configuration/TSConfig/page.ts
+++ Configuration/TSConfig/page.ts
@@ -1,3 +1,78 @@
+TCEMAIN {
+	translateToMessage = Bitte in "%s" übersetzen:
+	table {
+		pages {
+			disablePrependAtCopy = 1
+			disableHideAtCopy = 1
+		}
+		tt_content {
+			disableHideAtCopy = 1
+			disablePrependAtCopy = 1
+		}
+	}
+
+	permissions {
+		// Die Gruppe "Basis" wird beim Erstellen einer neuen
+		// Seite als Standard-Gruppe eingestellt
+		#groupid = 5
+
+		// Gruppen-Rechte werden um "löschen" erweitert
+		#group = 31
+	}
+}
+
+mod {
+	SHARED {
+		defaultLanguageFlag = gb
+		defaultLanguageLabel = english
+	}
+
+	web_layout {
+		defLangBinding = 1
+	}
+
+	web_list {
+		alternateBgColors = 1
+		#listOnlyInSingleTableView = 1
+	}
+}
+
+TCEFORM {
+	tt_content {
+		// Breite des RTE in Fullscreen-Ansicht
+		bodytext.RTEfullScreenWidth= 80%
+		section_frame {
+			// 10 (Einrücken) drinlassen
+			#removeItems = 1,5,6,11,12,20,21
+		}
+
+		header_layout {
+			altLabels {
+				#1 = Groesse 1
+				#2 = Groesse 2
+				#3 = Groesse 3
+				#4 = grafische Überschrift
+				#5 = normale Überschrift
+			}
+
+			#removeItems = 0,1,2,3
+
+			addItems {
+				#6 = grafische Boxen-Überschrift
+			}
+		}
+
+		image_effects {
+			addItems {
+				30 = Zuschneiden
+			}
+
+			// alle Standard-Effekte entfernen
+			removeItems = 1,2,3,10,11,20,23,25,26
+		}
+	}
+}
+
 // RTE Classe des Interface (Ausrichtung)
 RTE {
 	classes {
--- Configuration/TypoScript/TSConfig/Page/index.ts
+++
@@ -1,4 +0,0 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/tcemain.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/mod.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/tceform.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:vantomas/Configuration/TypoScript/TSConfig/Page/rte.ts">
