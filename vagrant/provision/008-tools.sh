45f119a2b10d5830f2132db16746e0eea2e70edb
Tommy Juhnke <tommy@van-tomas.de>
2014-04-06 16:14:11 +0200

    [TASK] explicitely installing nodejs + setting /usr/bin/node symlink

--- vagrant/provision/008-tools.sh
+++ vagrant/provision/008-tools.sh
@@ -3,7 +3,10 @@
 echo "Installing tools..."
 
 apt-get -y -qq install git curl libxml2-utils
-apt-get -y -qq install npm rubygems
+apt-get -y -qq install nodejs npm rubygems
+
+# @see http://justinvoelkel.me/problem-solved-foundation-5-cant-find-nodejs/
+ln -f -s /usr/bin/nodejs /usr/bin/node
 
 npm install -g bower grunt-cli
 gem install compass
