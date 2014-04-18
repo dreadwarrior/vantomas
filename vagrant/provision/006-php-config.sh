80171699b398bbb41d1e0c8aabff6c4c8153b67e
Tommy Juhnke <tommy@van-tomas.de>
2014-04-18 22:10:40 +0200

    [TASK] fine-tuning php5-fpm + nginx

--- vagrant/provision/006-php-config.sh
+++ vagrant/provision/006-php-config.sh
@@ -5,6 +5,8 @@ echo "Injecting PHP5 configuration..."
 # date.timezone setting
 ln -f -s /vagrant/vagrant/config/php5-cli/conf.d/01-php.ini /etc/php5/cli/conf.d/01-php.ini 
 
+# php.ini overrides
+ln -f -s /vagrant/vagrant/config/php5-fpm/conf.d/00-php.ini /etc/php5/fpm/conf.d/00-php.ini
 # opcache adjustments, see http://forge.typo3.org/issues/51475
 ln -f -s /vagrant/vagrant/config/php5-fpm/conf.d/06-opcache.ini /etc/php5/fpm/conf.d/06-opcache.ini
 # additional xdebug settings
