180fd6c3f71ab1e7b829fbbc29149a7f4c324b4a
Thomas Juhnke <tommy@van-tomas.de>
2015-05-05 10:07:13 +0200

    [TASK] Add apigen/apigen for API documentation generation

--- .provision/shell/004-php.sh
+++ .provision/shell/004-php.sh
@@ -2,7 +2,7 @@
 
 echo "Installing PHP..."
 
-install_packages php5-fpm php5 php5-cli php5-curl php5-gd php5-json php5-mcrypt php5-mysql php5-sqlite php5-xdebug php5-xsl php5-apcu
+install_packages php5-fpm php5 php5-cli php5-curl php5-gd php5-json php5-mcrypt php5-mysql php5-sqlite php5-xdebug php5-xsl php5-apcu php5-intl
 
 echo "Injecting PHP5 configuration..."
 
