22409ae728524d4bb89322787714f0bc71379c95
Thomas Juhnke <tommy@van-tomas.de>
2015-05-16 19:46:24 +0200

    [BUGFIX] Fix gitignore (sql excluded via global gitignore)

---
+++ web/typo3conf/ext/vantomas/ext_tables.sql
@@ -0,0 +1,18 @@
+#
+# Table structure for table 'tx_vantomas_domain_model_secretsanta_pair'
+#
+CREATE TABLE tx_vantomas_domain_model_secretsanta_pair (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+
+	donor int(11) DEFAULT '0' NOT NULL,
+	donee int(11) DEFAULT '0' NOT NULL,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
