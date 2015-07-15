38633e5af7fd2d94f57cfa757e8b33f43b6ed250
Thomas Juhnke <tommy@van-tomas.de>
2015-07-16 00:38:27 +0200

    [TASK] Add critical css optimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,13 +60,20 @@ module.exports = function(grunt) {
             'bower_components/layzr.js/dist/layzr.min.js'
           ]
         }
+      },
+      loadcss: {
+        files: {
+          'web/typo3conf/ext/vantomas/Resources/Public/Javascript/vendor/loadcss.min.js': [
+            'bower_components/loadcss/loadCSS.js'
+          ]
+        }
       }
     },
 
     watch: {
       grunt: {
           options: {
-              reload: true,
+              reload: true
           },
           files: ['Gruntfile.js']
       },
@@ -75,13 +82,100 @@ module.exports = function(grunt) {
         files: 'web/typo3conf/ext/vantomas/Resources/Private/Sass/**/*.scss',
         tasks: ['sass']
       }
+    },
+
+    critical: {
+      small: {
+        options: {
+          base: './',
+          css: [
+            'web/typo3conf/ext/vantomas/Resources/Public/Css/app.css'
+          ],
+          width: 640,
+          height: 480,
+          minify: true
+        },
+        src: '/tmp/index.html',
+        dest: 'web/typo3conf/ext/vantomas/Resources/Public/CriticalCss/small.css'
+      },
+      medium: {
+        options: {
+          base: './',
+          css: [
+            'web/typo3conf/ext/vantomas/Resources/Public/Css/app.css'
+          ],
+          width: 1024,
+          height: 768,
+          minify: true
+        },
+        src: '/tmp/index.html',
+        dest: 'web/typo3conf/ext/vantomas/Resources/Public/CriticalCss/medium.css'
+      },
+      large: {
+        options: {
+          base: './',
+          css: [
+            'web/typo3conf/ext/vantomas/Resources/Public/Css/app.css'
+          ],
+          width: 1440,
+          height: 1080,
+          minify: true
+        },
+        src: '/tmp/index.html',
+        dest: 'web/typo3conf/ext/vantomas/Resources/Public/CriticalCss/large.css'
+      },
+      xlarge: {
+        options: {
+          base: './',
+          css: [
+            'web/typo3conf/ext/vantomas/Resources/Public/Css/app.css'
+          ],
+          width: 1920,
+          height: 1200,
+          minify: true
+        },
+        src: '/tmp/index.html',
+        dest: 'web/typo3conf/ext/vantomas/Resources/Public/CriticalCss/xlarge.css'
+      },
+      xxlarge: {
+        options: {
+          base: './',
+          css: [
+            'web/typo3conf/ext/vantomas/Resources/Public/Css/app.css'
+          ],
+          width: 4096,
+          height: 3072,
+          minify: true
+        },
+        src: '/tmp/index.html',
+        dest: 'web/typo3conf/ext/vantomas/Resources/Public/CriticalCss/xxlarge.css'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-critical');
+
+  grunt.registerTask('wget_homepage', function() {
+    var done = this.async();
+
+    grunt.util.spawn({
+      cmd: ['wget'],
+      args: ['http://localhost', '-O /tmp/index.html'],
+      opts: {
+        stdio: 'inherit'
+      }
+    }, function doneFunction(err, result, code) {
+      //if (err) throw err;
+      grunt.log.ok('Successfully scraped homepage to /tmp/index.html');
+
+      done();
+    });
+  });
 
   grunt.registerTask('build', ['sass', 'uglify']);
+  grunt.registerTask('ccss', ['wget_homepage', 'critical']);
   grunt.registerTask('default', ['build','watch']);
 };
