b65ce1b4e0fe9f2a9158145283e3e0b6deccb6dd
Thomas Juhnke <tommy@van-tomas.de>
2015-08-05 00:12:56 +0200

    [TASK] Introduce StoragePageId ViewHelper widget
    This helps propagating the tt_content record's `pages` field into the
    template without polluting a non-widget controller with access to the
    ContentObjectRender instance within the ConfigurationManager instance.

---
+++ web/typo3conf/ext/vantomas/Classes/ViewHelpers/Widget/Controller/StoragePageIdController.php
@@ -0,0 +1,36 @@
+<?php
+namespace DreadLabs\Vantomas\ViewHelpers\Widget\Controller;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Fluid\Core\Widget\AbstractWidgetController;
+
+/**
+ * StoragePageIdController
+ *
+ * @author Thomas Juhnke <typo3@van-tomas.de>
+ */
+class StoragePageIdController extends AbstractWidgetController {
+
+	/**
+	 * Renders the widget
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+		$storagePageId = (int) $this->configurationManager->getContentObject()->data['pages'];
+
+		$this->view->assign('storagePageId', $storagePageId);
+	}
+}
