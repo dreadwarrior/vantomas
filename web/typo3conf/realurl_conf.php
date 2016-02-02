87098889b5e2a08583118fc04c131cb39b0f95dc
Thomas Juhnke <tommy@van-tomas.de>
2016-02-02 22:37:13 +0100

    [TASK] Streamline realurl configuration (remove /index.html); adjust nginx configuration

--- web/typo3conf/realurl_conf.php
+++ web/typo3conf/realurl_conf.php
@@ -1,173 +1,132 @@
 <?php
-$TYPO3_CONF_VARS['EXTCONF']['realurl'] = array(
-	'_DEFAULT' => array(
-		'init' => array(
-			'enableCHashCache' => TRUE,
-			'appendMissingSlash' => 'ifNotFile',
-			'enableUrlDecodeCache' => TRUE,
-			'enableUrlEncodeCache' => TRUE,
-			'emptyUrlReturnValue' => '/',
-			'reapplyAbsRefPrefix' => TRUE,
-		),
-		'preVars' => array(
-			/*
-			array(
-				'GETvar' => 'L',
-				'valueMap' => array(
-					'de' => '0',
-					'en' => '1'
-				),
-				'valueDefault' => 'de',
-			),
-			*/
-		),
-		'pagePath' => array(
-			'type' => 'user',
-			'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
-			'spaceCharacter' => '-',
-			'languageGetVar' => 'L',
-			'expireDays' => 30,
-			'rootpage_id' => 136,
-		),
-		'fixedPostVars' => array(
-		),
-		'postVarSets' => array(
-			'_DEFAULT' => array(
-				// dont use no_cache in preVars
-				// @see http://typo3bloke.net/post-details/archive/2006/september/12/do_not_use_no_cache_as_prevar/index.htm
-				/*
-				'nc' => array(
-					'type' => 'single',
-					'GETvar' => 'no_cache',
-				),
-				*/
+$TYPO3_CONF_VARS['EXTCONF']['realurl'] = [
+    '_DEFAULT' => [
+        'init' => [
+            'enableCHashCache' => true,
+            // @see http://typo3.codeblocks.de/anleitungen-tipps/tutorials-codesnippets/realurl/trailing-slash-duplicate-content/
+            'appendMissingSlash' => 'ifNotFile,redirect[301]',
+            'enableUrlDecodeCache' => true,
+            'enableUrlEncodeCache' => true,
+            'emptyUrlReturnValue' => '/',
+            'reapplyAbsRefPrefix' => true,
+        ],
+        'preVars' => [
+        ],
+        'pagePath' => [
+            'type' => 'user',
+            'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
+            'spaceCharacter' => '-',
+            'languageGetVar' => 'L',
+            'expireDays' => 30,
+            'rootpage_id' => 136,
+        ],
+        'fixedPostVars' => [
+        ],
+        'postVarSets' => [
+            '_DEFAULT' => [
+                // archive - START
+                'archive-date' => [
+                    ['GETvar' => 'tx_vantomas_archivesearch[month]'],
+                    ['GETvar' => 'tx_vantomas_archivesearch[year]'],
+                    [
+                        'GETvar' => 'tx_vantomas_archivesearch[controller]',
+                        'noMatch' => 'bypass'
+                    ],
+                    [
+                        'GETvar' => 'tx_vantomas_archivesearch[action]',
+                        'noMatch' => 'bypass'
+                    ],
+                ],
+                // archive - END
 
-				// archive - START
-				'archive-date' => array(
-					array('GETvar' => 'tx_vantomas_archivesearch[month]'),
-					array('GETvar' => 'tx_vantomas_archivesearch[year]'),
-					array(
-						'GETvar' => 'tx_vantomas_archivesearch[controller]',
-						'noMatch' => 'bypass'
-					),
-					array(
-						'GETvar' => 'tx_vantomas_archivesearch[action]',
-						'noMatch' => 'bypass'
-					),
-				),
-				// archive - END
+                'tag' => [
+                    [
+                        'GETvar' => 'tx_vantomas_tagsearch[controller]',
+                        'noMatch' => 'bypass'
+                    ],
+                    [
+                        'GETvar' => 'tx_vantomas_tagsearch[action]',
+                        'valueMap' => [],
+                        'noMatch' => 'bypass'
+                    ],
+                    [
+                        'GETvar' => 'tx_vantomas_tagsearch[tag]'
+                    ],
+                ],
 
-				'tag' => array(
-					array(
-						'GETvar' => 'tx_vantomas_tagsearch[controller]',
-						'noMatch' => 'bypass'
-					),
-					array(
-						'GETvar' => 'tx_vantomas_tagsearch[action]',
-						'valueMap' => array(),
-						'noMatch' => 'bypass'
-					),
-					array(
-						'GETvar' => 'tx_vantomas_tagsearch[tag]'
-					),
-				),
+                // tx_vantomas_contactform[action]=success&tx_vantomas_contactform[controller]=Form&cHash=...
+                'contact-form' => [
+                    [
+                        'GETvar' => 'tx_vantomas_contactform[controller]',
+                        'valueMap' => [],
+                        'noMatch' => 'bypass',
+                    ],
+                    [
+                        'GETvar' => 'tx_vantomas_contactform[action]',
+                        'valueMap' => [
+                            'send' => 'send',
+                            'success' => 'success',
+                        ],
+                        'noMatch' => 'bypass',
+                    ],
+                ],
 
-				// tx_vantomas_contactform[action]=success&tx_vantomas_contactform[controller]=Form&cHash=...
-				'contact-form' => array(
-					array(
-						'GETvar' => 'tx_vantomas_contactform[controller]',
-						'valueMap' => array(),
-						'noMatch' => 'bypass',
-					),
-					array(
-						'GETvar' => 'tx_vantomas_contactform[action]',
-						'valueMap' => array(
-							'send' => 'send',
-							'success' => 'success',
-						),
-						'noMatch' => 'bypass',
-					),
-				),
-
-				// tx_vantomas_secretsanta[action]=loginForm&tx_vantomas_secretsanta[controller]=SecretSanta&cHash=...
-				'wichtel-zugang' => array(
-					array(
-						'GETvar' => 'tx_vantomas_secretsantaaccesscontrol[controller]',
-						'valueMap' => array(),
-						'noMatch' => 'bypass',
-					),
-					array(
-						'GETvar' => 'tx_vantomas_secretsantaaccesscontrol[action]',
-						'valueMap' => array(
-							'anmelden' => 'login',
-							'abmelden' => 'logout',
-						),
-						'noMatch' => 'bypass',
-					),
-				),
-				'wichtel' => array(
-					array(
-						'GETvar' => 'tx_vantomas_secretsanta[controller]',
-						'valueMap' => array(),
-						'noMatch' => 'bypass',
-					),
-					array(
-						'GETvar' => 'tx_vantomas_secretsanta[action]',
-						'valueMap' => array(
-							'anzeigen' => 'show',
-						),
-						'noMatch' => 'bypass',
-					),
-				),
-			)
-		),
-		'fileName' => array (
-			'index' => array(
-				'rss.xml' => array(
-					'keyValues' => array(
-						'type' => 100
-					)
-				),
-				'rss091.xml' => array(
-					'keyValues' => array(
-						'type' => 101
-					)
-				),
-				'rdf.xml' => array(
-					'keyValues' => array(
-						'type' => 102
-					)
-				),
-				'atom.xml' => array(
-					'keyValues' => array(
-						'type' => 103
-					)
-				),
-				'sitemap.xml' => array(
-					'keyValues' => array(
-						'type' => 666
-					)
-				),
-				'page.html' => array(
-					'keyValues' => array(
-						'type' => 1
-					)
-				),
-				'print.html' => array(
-					'keyValues' => array(
-						'print' => 1
-					)
-				),
-				'index.html' => array(
-					'keyValues' => array(),
-				),
-				'index.jsonld' => array(
-					'keyValues' => array(
-						'type' => 1453488849009,
-					),
-				),
-			),
-			'defaultToHTMLsuffixOnPrev' => 1,
-		),
-	),
-);
+                // tx_vantomas_secretsanta[action]=loginForm&tx_vantomas_secretsanta[controller]=SecretSanta&cHash=...
+                'wichtel-zugang' => [
+                    [
+                        'GETvar' => 'tx_vantomas_secretsantaaccesscontrol[controller]',
+                        'valueMap' => [],
+                        'noMatch' => 'bypass',
+                    ],
+                    [
+                        'GETvar' => 'tx_vantomas_secretsantaaccesscontrol[action]',
+                        'valueMap' => [
+                            'anmelden' => 'login',
+                            'abmelden' => 'logout',
+                        ],
+                        'noMatch' => 'bypass',
+                    ],
+                ],
+                'wichtel' => [
+                    [
+                        'GETvar' => 'tx_vantomas_secretsanta[controller]',
+                        'valueMap' => [],
+                        'noMatch' => 'bypass',
+                    ],
+                    [
+                        'GETvar' => 'tx_vantomas_secretsanta[action]',
+                        'valueMap' => [
+                            'anzeigen' => 'show',
+                        ],
+                        'noMatch' => 'bypass',
+                    ],
+                ],
+            ]
+        ],
+        'fileName' =>  [
+            'index' => [
+                'atom.xml' => [
+                    'keyValues' => [
+                        'type' => 103,
+                    ]
+                ],
+                'sitemap.xml' => [
+                    'keyValues' => [
+                        'type' => 666,
+                    ]
+                ],
+                '.jsonld' => [
+                    'keyValues' => [
+                        'type' => 1453488849009,
+                    ],
+                ],
+                '_DEFAULT' => [
+                    'keyValues' => [
+                    ],
+                ],
+            ],
+            'defaultToHTMLsuffixOnPrev' => false,
+            'acceptHTMLsuffix' => false,
+        ],
+    ],
+];
